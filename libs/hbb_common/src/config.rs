4e04e9148ee56ffdeb17f969de414b70f2497980
byslectcol-tech <byslectcol@gmail.com>
2025-09-17 23:19:03 +0800

    Update config.rs
    默认端口

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -103,8 +103,8 @@ const CHARS: &[char] = &[
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.sk728.com"];
 pub const RS_PUB_KEY: &str = "N45eu7E+ji4N7Cm8yUJDzGsxzhdI0qaYLQgoa6MR+ak=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 31116;
+pub const RELAY_PORT: i32 = 31117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
 pub const WS_RELAY_PORT: i32 = 21119;
 
